01dd5474a70b3e16e8b26f7aa6049a8e53cbef92
Preston-Wong <you@example.com>
2023-10-10 10:02:46 -0400

    commented

--- Sketch.java
+++ Sketch.java
@@ -3,10 +3,12 @@ import java.util.Random;
 
 public class Sketch extends PApplet {
 	
+  // randomizers
   Random myRandom = new Random();
   int intRandomX = myRandom.nextInt((500-20) + 1) + 20;
   int intRandomY = myRandom.nextInt((500-20) + 1) + 20;
   int intRandomZ = myRandom.nextInt((500-20) + 1) + 20;
+  
   public void settings() {
 	// put your size call here
     size(400, 400);
@@ -14,7 +16,8 @@ public class Sketch extends PApplet {
   }
 
   public void setup() {
-    
+
+    // changes background colour based on the randomizer values 
     if (intRandomZ <= 100) { 
       background(0, 0, 255);
     }
@@ -24,12 +27,27 @@ public class Sketch extends PApplet {
     else {
       background(0,0,0);
     }
+
   }
 
   public void draw() {
-    
+    // variables for the date 
+    int day = day();
+    int month = month();
+    int year = year();
+
+    // draws the circle 
     fill(intRandomZ + 100,intRandomZ + 100,intRandomZ * 0);
     ellipse(intRandomX,intRandomY,intRandomZ / 2,intRandomZ / 2);
+    
+    // displays the date when the program was runned
+    fill(255,255,255);
+    String s = String.valueOf(day);
+    text(s, 10, 28);
+    s = String.valueOf(month);
+    text(s, 10, 56); 
+    s = String.valueOf(year);
+    text(s, 10, 84);
   }
   
  
