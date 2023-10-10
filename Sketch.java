7f9aea6596538447e168d6679ca918ea54eff838
Preston-Wong <you@example.com>
2023-10-10 09:42:39 -0400

    Randomized colours

--- Sketch.java
+++ Sketch.java
@@ -3,27 +3,33 @@ import java.util.Random;
 
 public class Sketch extends PApplet {
 	
-	
-   Random myRandom = new Random();
-  int intRandom = myRandom.nextInt((300-20) + 1) + 20;
-
+  Random myRandom = new Random();
+  int intRandomX = myRandom.nextInt((500-20) + 1) + 20;
+  int intRandomY = myRandom.nextInt((500-20) + 1) + 20;
+  int intRandomZ = myRandom.nextInt((500-20) + 1) + 20;
   public void settings() {
 	// put your size call here
     size(400, 400);
-   
-    
+     
   }
-
 
   public void setup() {
     
-    background(210, 255, 173);
+    if (intRandomZ <= 100) { 
+      background(0, 0, 255);
+    }
+    else if ((intRandomZ > 100) && (intRandomZ <= 300)) {
+      background(0,0,128);
+    }
+    else {
+      background(0,0,0);
+    }
   }
 
   public void draw() {
     
-    fill(204,100,0);
-    ellipse(intRandom,intRandom,intRandom/2,intRandom/2);
+    fill(intRandomZ + 100,intRandomZ + 100,intRandomZ * 0);
+    ellipse(intRandomX,intRandomY,intRandomZ / 2,intRandomZ / 2);
   }
   
  
